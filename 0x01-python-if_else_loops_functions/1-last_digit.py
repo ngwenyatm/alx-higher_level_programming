53f10c19f69a834619bb63bfd6a42bf8901ed53c
ngwenyatm <thabsnspaces@gmail.com>
2023-11-28 13:17:25 -0800

    pushed with script

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 0:
-    trailNum = number % 10
+if number >= 0:
+    trailNum = (number % 10)
 elif number < 0:
-    trailNum = number % -10
+    trailNum = (number % -10)
 if trailNum > 5:
     print(f"Last digit of {number} is {trailNum} and is greater than 5")
 elif trailNum == 0:
