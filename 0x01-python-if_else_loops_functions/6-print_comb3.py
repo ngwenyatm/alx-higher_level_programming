aa19f410b9b51f87681af926493455edf634b801
ngwenyatm <thabsnspaces@gmail.com>
2023-11-28 12:44:34 -0800

    pushed with script

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for i in range(10):
     for j in range(1,10):
         if i >= j:
             continue
-        elif i == 8 and j == 9;
-        print("{}{}".format(i,j)
+        elif i == 8 and j == 9:
+        print("{}{}".format(i, j)
                 else:
-                print("{}{}".format(i,j), end=", ")
+                print("{}{}".format(i, j), end=", ")
