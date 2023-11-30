0a16cc2efd5b2e9077565bbb4fb6b7dcef40e3c4
ngwenyatm <thabsnspaces@gmail.com>
2023-11-30 12:01:48 -0800

    pushed with script

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
     total = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            total += int(arg)
+    for arg in sys.argv[1:]:
+        total += int(arg)
     print(total)
