2636362f498c2492339544417752c206a0c758a1
ngwenyatm <thabsnspaces@gmail.com>
2023-11-30 11:13:15 -0800

    pushed with script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    numArg = len(sys.argv)-1
-    if numArg == 1:
-        print("{} arguments.".format(numArg))
-    elif numArg == 2:
-        print("{} argument:".format(numArg))
+    argLen = len(sys.argv)
+    if argLen == 1:
+        print("{} arguments.".format(argLen - 1))
+    elif argLen == 2:
+        print("{} argument:".format(argLen - 1))
     else:
-        print("{} arguments:".format(numArg))
-    for i in range(1, numArg):
+        print("{} arguments:".format(argLen - 1))
+    for i in range(1, argLen):
         print("{}: {}".format(i, sys.argv[i]))
+
