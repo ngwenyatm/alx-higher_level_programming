091354c2cc16d1e4374f1d6841d13d742b9f3330
ngwenyatm <thabsnspaces@gmail.com>
2023-12-04 11:14:22 -0800

    pushed with script

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 def multiple_returns(sentence):
    str_len = len(sentence)
     fchar = sentence[0]
-    if str_len > 0 
-    else "None"
+    if str_len > 0 else "None"
     retup = str_len, fchar
     return(retup)
