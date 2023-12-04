f5b7eec5e2ec9b14b2f2e5d91d3976c6537c0c1c
ngwenyatm <thabsnspaces@gmail.com>
2023-12-04 10:06:10 -0800

    pushed with script

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         print()
     else:
-        for row in range(len(matrix)):
+        for cur_row in range(len(matrix)):
             for value in range(len(matrix[cur_row])):
                 if value != len(matrix[cur_row]) - 1:
                     endspace = ' '
