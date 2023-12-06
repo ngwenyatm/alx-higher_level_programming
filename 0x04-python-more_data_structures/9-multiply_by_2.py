4d896440ca90a1ad8d295b8743f0c7c72872242e
ngwenyatm <thabsnspaces@gmail.com>
2023-12-06 08:06:19 -0800

    pushed with script

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    newDict = a_dictionary.copy()
+    keylist = list(newDict .keys())
+    for i in keylist :
+        newDict [i] *= 2
+    return (newDict )
