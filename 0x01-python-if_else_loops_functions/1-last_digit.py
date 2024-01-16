88e436a9e0d73ef52a4bc1cb035ffc86f958aecd
ngwenyatm <thabsnspaces@gmail.com>
2024-01-16 06:42:03 -0800

    pushed with script

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number >= 0:
-    trailNum = (number % 10)
+    lastNambah = (number % 10)
 elif number < 0:
-    trailNum = (number % -10)
-if trailNum > 5:
-    print(f"Last digit of {number} is {trailNum} and is greater than 5")
-elif trailNum == 0:
-    print(f"Last digit of {number} is {trailNum} and is 0")
+    lastNambah = (number % -10)
+if lastNambah > 5:
+    print(f"Last digit of {number} is {lastNambah} and is greater than 5")
+elif lastNambah == 0:
+    print(f"Last digit of {number} is {lastNambah} and is 0")
 else:
-    print(f"Last digit of {number} is {trailNum} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {lastNambah} and is less than 6 and not 0")
