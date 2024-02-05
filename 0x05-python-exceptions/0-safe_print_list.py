551ae303e242961d4bf4455434060d9f71ec8bec
ngwenyatm <thabsnspaces@gmail.com>
2024-02-05 11:44:33 -0800

    fix task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    num = 0
+    n = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
-            num += 1
+            n += 1
         except Error:
             break
     print("")
-    Return
-i
+    return n
