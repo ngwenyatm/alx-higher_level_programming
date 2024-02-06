5033e1034d0290cdea0c24a72a2d3e1000ee9fe6
ngwenyatm <thabsnspaces@gmail.com>
2024-02-06 10:40:52 -0800

    first 3 test

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module for defining a square"""
+
+class Square:
+    """A square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A Square module"""
+
+
+class Square:
+    """what's in a square."""
+
+    def __init__(self, size):
+        """Initialize a square
+
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
