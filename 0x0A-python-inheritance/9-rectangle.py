424f766a17f52ccdea43baa8f08d0811466fb70b
ngwenyatm <thabsnspaces@gmail.com>
2024-02-19 13:56:36 -0800

    pushed with script

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,17 +2,39 @@
 """class Rectangle inherited from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        super().__init__()
-        self.__width = integer_validator("width", width)
-        self.__height = integer_validator("height", height)
+class BaseGeometry:
+    """Class with public methods area and integer_validator"""
+
+    def __init__(self):
+        pass
+
+    def integer_validator(cls, name, value):
+        """checks that value is greater than 0"""
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
     def area(self):
+        """Raises exception when called"""
+        raise NotImplementedError("area() is not implemented")
+
+
+class Rectangle(BaseGeometry):
+    """A representation of a rectangle"""
+
+    def __init__(self, width, height):
+        """rectangle initialization"""
+        super().__init__()
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns area"""
         return self.__width * self.__height
 
     def __str__(self):
+        """string representation of the rectangle"""
         return f"[Rectangle] {self.__width}/{self.__height}"
-
-    def __repr__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
