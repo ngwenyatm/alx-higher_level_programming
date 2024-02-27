587f561805851d6fca415ff4eb416855901d6132
ngwenyatm <thabsnspaces@gmail.com>
2024-02-27 11:32:29 -0800

    pushed with script

--- 0x0C-python-almost_a_circle/rectangle.py
+++ 0x0C-python-almost_a_circle/rectangle.py
@@ -22,35 +22,26 @@ class Rectangle(Base):
     @property
     def height(self):
         return self.__height
-
+    @height.setter
+    def height(self, height):
+        self.__height = height
     @property
     def width(self):
         return self.__width
-
+    @width.setter
+    def width(self, width):
+        self.__width = width
     @property
     def y(self):
         return self.__y
-
+    @y.setter
+    def y(self, y):
+        self.__y = y
     @property
     def x(self):
         return self.__x
 
-    @height.setter
-    def height(self, height):
-        self.__height = height
-
-    @width.setter
-    def width(self, width):
-        self.__width = width
-
     @x.setter
     def x(self, x):
         self.__x = x
-
-    @y.setter
-    def y(self, y):
-        self.__y = y
-
-
-
 
