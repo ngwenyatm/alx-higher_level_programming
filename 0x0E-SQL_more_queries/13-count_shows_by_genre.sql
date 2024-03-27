8603495254b12026da1434d0d14a28938d3ef4c3
ngwenyatm <thabsnspaces@gmail.com>
2024-03-27 10:59:45 -0700

    pushed with script

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all genres from hbtn_0d_tvshows
+-- displays the number of shows linked to each.
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
